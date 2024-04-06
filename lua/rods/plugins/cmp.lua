0240c047d503f82f615a8f8f71cf2937a5c533b5
Rodrigo Stuchi <rod.stuchi@gmail.com>
2024-04-06 13:49:30 -0300

    nvim-cmp: update c-k, -j keymaps

--- lua/rods/plugins/cmp.lua
+++ lua/rods/plugins/cmp.lua
@@ -11,11 +11,16 @@ return {
 		{ "ray-x/cmp-treesitter" },
 	},
 	config = function()
+		local has_words_before = function()
+			unpack = unpack or table.unpack
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
 		table.unpack = table.unpack or unpack -- 5.1 compatibility
 
 		local luasnip = require("luasnip")
 		local cmp = require("cmp")
-		local cmp_select_opts = { behavior = cmp.SelectBehavior.Select }
+		local cmp_select_opts = { behavior = cmp.SelectBehavior.Select, count = 1 }
 
 		cmp.setup({
 			completion = {
@@ -42,18 +47,25 @@ return {
 				["<C-e>"] = cmp.mapping.abort(),
 				["<C-u>"] = cmp.mapping.scroll_docs(-4),
 				["<C-d>"] = cmp.mapping.scroll_docs(4),
-				["<c-k>"] = cmp.mapping(function()
-					if luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else --if cmp.visible() then
+				["<c-k>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
 						cmp.select_prev_item(cmp_select_opts)
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
 					end
 				end, { "i", "s" }),
-				["<c-j>"] = cmp.mapping(function()
-					if luasnip.expand_or_jumpable() then
+				["<c-j>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
 						luasnip.expand_or_jump()
-					else
+					elseif has_words_before() then
+						-- cmp.complete()
 						cmp.select_next_item(cmp_select_opts)
+					else
+						fallback()
 					end
 				end, { "i", "s" }),
 				["<c-l>"] = cmp.mapping(function()
